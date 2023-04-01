3731062901a644d158d7f31cdc266dbacbe76729
Crislan Cruz <crislancruz@hotmail.com>
2023-04-01 19:03:03 -0400

    Feat: Usuario model

---
+++ src/model/Usuario.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const usuarioSchema = new mongoose.Schema({
+    nome: {type: String, required: true},
+    email: {type: String, unique: true, required: true},
+    senha: {type: String, required: true}, 
+    imagem: {type: String, required: true},
+    enderecos: [
+        {
+            logradouro: {type: String, required: true},
+            numero: {type: Number, required: true},
+            complemento: {type: String, required: false},
+            cep: {type: String, required: true},
+            createdAt: {type: Date, required: true}
+        }
+    ],
+    produtos_fav: [
+        {
+            _id: {type: mongoose.Types.ObjectId, required: true, unique: true, ref: "produtos"}
+        }
+    ],
+    admin: {type: Boolean, required: true, default: false},
+    createdAt: {type: Date, required: true}
+});
+
+const Usuario = mongoose.model("usuarios", usuarioSchema);
+
+module.exports = Usuario;
