9c9d72609533582af03946910f2e75a25d26b5f2
Crislan Cruz <crislancruz@hotmail.com>
2023-04-02 12:02:33 -0400

    Fix: createAt

--- src/model/Usuario.js
+++ src/model/Usuario.js
@@ -10,16 +10,17 @@ const usuarioSchema = new mongoose.Schema({
             numero: {type: Number, required: true},
             complemento: {type: String, required: false},
             cep: {type: String, required: true},
-            createdAt: {type: Date, required: true}
+            createdAt: {type: Date, required: true, default: Date.now()}
         }
     ],
     produtos_fav: [
         {
-            _id: {type: mongoose.Types.ObjectId, required: true, unique: true, ref: "produtos"}
+            _id: {type: mongoose.Types.ObjectId, required: true, unique: true, ref: "produtos"},
+            createdAt: {type: Date, required: true, default: Date.now()}
         }
     ],
     admin: {type: Boolean, required: true, default: false},
-    createdAt: {type: Date, required: true}
+    createdAt: {type: Date, required: true, default: Date.now()}
 });
 
 const Usuario = mongoose.model("usuarios", usuarioSchema);
