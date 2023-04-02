bdf5fa6d066b028c9604748ae64707cdd9e8636d
Crislan Cruz <crislancruz@hotmail.com>
2023-04-02 17:21:56 -0400

    Feat: criptografia na senha do User

--- src/model/Usuario.js
+++ src/model/Usuario.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
-const usuarioSchema = new mongoose.Schema({
+const bcrypt = require("bcrypt");
+
+const UsuarioSchema = new mongoose.Schema({
     nome: {type: String, required: true},
     email: {type: String, unique: true, required: true},
     senha: {type: String, required: true}, 
@@ -23,6 +25,13 @@ const usuarioSchema = new mongoose.Schema({
     createdAt: {type: Date, required: true, default: Date.now()}
 });
 
-const Usuario = mongoose.model("usuarios", usuarioSchema);
+UsuarioSchema.pre("save", async function(next){
+    if(this.senha){
+        this.senha = await bcrypt.hash(this.senha, 10);
+    }
+    next();
+});
+
+const Usuario = mongoose.model("usuarios", UsuarioSchema);
 
 module.exports = Usuario;
